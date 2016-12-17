acd946184eef9e7ec3b205af64ac426c23602f91
Svenn-Arne Dragly <s@dragly.com>
2016-12-17 15:23:51 +0100

    Add travis test

--- pyxona/tests/test_core.py
+++ pyxona/tests/test_core.py
@@ -10,6 +10,9 @@ def test_core():
         path = "/home/milad/Dropbox/cinpla-shared/project/axonaio/2016-03-02-083928-1596/raw/02031602.set"
     elif username == "svenni":
         path = "/home/svenni/Dropbox/studies/cinpla/cinpla-shared/project/axonaio/2016-03-02-083928-1596/raw/02031602.set"
+    else:
+        print("Sorry, tests are not yet available.")
+        exit(0)
     axona_file = pyxona.File(path)
 
     for analog_signal in axona_file.analog_signals:
