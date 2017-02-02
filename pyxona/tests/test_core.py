d8ba81b21e6c9feae2eb26ccf6e5d7093ac1ebd5
Milad <miladh@ibv.uio.no>
2017-02-02 18:11:53 +0100

    travis conda updates

--- pyxona/tests/test_core.py
+++ pyxona/tests/test_core.py
@@ -14,6 +14,7 @@ def _check_array_equal(a, b):
         return ((a == b) | (np.isnan(a) & np.isnan(b))).all()
 
 
+
 # def test_spike_train_reader():
 #     axona_file = pyxona.File(axona_file_path)
 # 
